cf40862afc222dabb161ba0606c1f1b67f0f7f2f
DymaxionMap <sthliao@gmail.com>
2018-11-16 15:45:52 -0800

    Added AJAX call to fetch messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -14,6 +14,21 @@ var App = {
     // Fetch initial batch of messages
     App.startSpinner();
     App.fetch(App.stopSpinner);
+    
+    // TEMPORARY to check out how to GET from server
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed');
+      }
+    });
 
   },
 
